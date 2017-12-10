850edd081d9264cb911b282862d842f7bb5efd4c
DESKTOP-CF5R1ST\H-cym <cgd214@gmail.com>
2017-12-10 15:03:13 +0800

    config 副本

---
+++ config_copy.py
@@ -0,0 +1,4 @@
+# 用于 Flask 框架的密钥，用于 session 加密，用os.urandom(24)生成
+SECRET_KEY = '\x98y\xf4jU\xad\xe1\x08Z)\x08\x17\x07\n\x1d\x16\xcb\xe6\xd3xJx\xebH'
+# 公共聊天房间列表
+PUBLIC_ROOMS = ['大厅', '游戏', '影视', '技术']
